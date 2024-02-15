f217e9bb6711693e71bc447e6312b6844453ce05
Mehul Ved <mehul.n.ved@gmail.com>
2024-02-15 15:59:42 +0530

    Move to modular terraform code

--- dev/app/providers.tf
+++ dev/app/providers.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.25.2"
-    }
-  }
-
   required_version = "~> 1.7.0"
 }
 
@@ -14,12 +7,4 @@ data "terraform_remote_state" "kubernetes_cluster" {
   config = {
     path = "../cluster/terraform.tfstate"
   }
-}
-
-provider "kubernetes" {
-  host = data.terraform_remote_state.kubernetes_cluster.outputs.cluster_host
-
-  client_certificate     = data.terraform_remote_state.kubernetes_cluster.outputs.client_certificate
-  client_key             = data.terraform_remote_state.kubernetes_cluster.outputs.client_key
-  cluster_ca_certificate = data.terraform_remote_state.kubernetes_cluster.outputs.cluster_ca_certificate
 }
--- dev/app/variables.tf
+++ dev/app/variables.tf
@@ -1,46 +1,19 @@
 variable "environment" {
-  description = "Environment Name"
-  type        = string
-  default     = "dev"
-
-  validation {
-    condition     = contains(["dev", "stg", "prod"], var.environment)
-    error_message = "Valid Environments - dev, stg, prod"
-  }
-}
-
-variable "domain" {
-  description = "TLD for the apps"
-  type        = string
-  default     = "one2n.local"
-}
-
-variable "storage_name" {
-  description = "Name of the storage volume"
-  type        = string
-  default     = "app-storage"
-}
-
-variable "storage_requests" {
-  description = "Requested storage for PV claim"
-  type        = string
-  default     = "5Gi"
-}
-
-variable "storage_capacity" {
-  description = "Storage Capacity for PV claim"
-  type        = string
-  default     = "10Gi"
+  type    = string
+  default = "dev"
 }
 
 variable "app_name" {
-  description = "Name of the app"
-  type        = string
-  default     = "app"
+  type    = string
+  default = "app"
 }
 
-variable "app_port" {
-  description = "Application Port"
-  type        = number
-  default     = 80
+variable "domain" {
+  type    = string
+  default = "one2n.local"
+}
+
+variable "namespace" {
+  type    = string
+  default = "apps"
 }
